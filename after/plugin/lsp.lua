989ed6b47ba3bfc4138b1334b998400e73bd108d
Javier Neri <javierneri81@gmail.com>
2023-03-29 15:49:56 +0800

    fix: add stylua formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,10 +1,9 @@
-local lsp = require('lsp-zero').preset({
-    name = 'minimal',
+local lsp = require("lsp-zero").preset({
+    name = "minimal",
     set_lsp_keymaps = true,
     manage_nvim_cmp = true,
     suggest_lsp_servers = false,
 })
-
 
 local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
@@ -15,33 +14,25 @@ lsp.nvim_workspace()
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+    virtual_text = true,
 })
-
-
 
 null_ls.setup({
     sources = {
+        null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.completion.spell,
     },
-    on_attach = function(client, bufrn)
+    on_attach = function(client, bufnr)
         if client.supports_method("textDocument/formatting") then
             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
             vim.api.nvim_create_autocmd("BufWritePre", {
                 group = augroup,
                 buffer = bufnr,
                 callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr,
-                    filter = function(_client)
-                        return _client.name == "null-ls"
-                     end
-                })
+                    vim.lsp.buf.format({ bufnr = bufnr })
                 end,
             })
         end
     end,
 })
-
-
