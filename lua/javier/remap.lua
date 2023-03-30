e03905656c84a396859ebc78f4a5f7420b1f1cd9
Javier Neri <javierneri81@gmail.com>
2023-03-30 15:43:24 +0800

    feat(remap): add remap for saving to system clipboard

--- lua/javier/remap.lua
+++ lua/javier/remap.lua
@@ -10,9 +10,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
-vim.keymap.set("n","<leader>y", "\"+y")
-vim.keymap.set("v","<leader>y", "\"+y")
-vim.keymap.set("n","<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("n", "<C-s>", vim.cmd.w)
 
