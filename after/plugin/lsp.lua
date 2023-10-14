531e2646a5851588a9be419f2481eac6f9d06cb9
Javier Neri <javierneri81@gmail.com>
2023-10-14 23:24:18 +0800

    feat: add astro filetype to prettier with null-ls

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,7 +16,13 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier.with({
+            filetypes = {
+                "astro", "javascript", "javascriptreact", "typescript",
+                "typescriptreact", "vue", "css", "scss", "less", "html", "json",
+                "jsonc", "yaml", "markdown", "markdown.mdx", "graphql", "handlebars"
+            }
+        }),
     },
 })
 
