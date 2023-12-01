3763eca778a04b899dec3d11494be6f7bd318f0e
Javier Neri <javierneri81@gmail.com>
2023-12-02 00:56:07 +0800

    fix: attach html lsp to astro files

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,6 +17,22 @@ require("mason-lspconfig").setup({
 			local lua_opts = lsp_zero.nvim_lua_ls()
 			lspconfig.lua_ls.setup(lua_opts)
 		end,
+		html = function()
+			lspconfig.html.setup({
+				filetypes = {
+					"css",
+					"eruby",
+					"html",
+					"htmldjango",
+					"javascriptreact",
+					"less",
+					"pug",
+					"sass",
+					"scss",
+					"typescriptreact",
+				},
+			})
+		end,
 	},
 })
 
