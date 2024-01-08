7a2fd0268033fdf2c935e0791aa4c2ef276a10af
Javier Neri <javierneri81@gmail.com>
2024-01-08 16:17:02 +0800

    fix: remove luasnip keymap for now since <leader> keymaps slowdown typing in insert mode

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -65,14 +65,7 @@ return {
 		"L3MON4D3/LuaSnip",
 		dependencies = { "rafamadriz/friendly-snippets" },
 		config = function()
-			local ls = require("luasnip")
 			require("luasnip.loaders.from_vscode").lazy_load()
-			vim.keymap.set({ "i", "s" }, "<leader>;", function()
-				ls.jump(1)
-			end, { silent = true })
-			vim.keymap.set({ "i", "s" }, "<leader>,", function()
-				ls.jump(-1)
-			end, { silent = true })
 		end,
 	},
 	{ "saadparwaiz1/cmp_luasnip" },
