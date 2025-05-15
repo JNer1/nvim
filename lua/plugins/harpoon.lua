2f4f560e62283c64f9e0565a48473331a68d8967
Javier Neri <javierneri81@gmail.com>
2025-05-16 01:37:19 +0800

    fix: harpoon would not save markers when exiting neovim

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,6 +2,7 @@ return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	commit = "e76cb03",
 	config = function()
 		local harpoon = require("harpoon")
 
